0e12f58cff6df8148d3a36e8a0ff8f6518701737
Orochi771127 <orochi771127@gmail.com>
2026-06-06 11:55:46 +0800

    Update sceneLayout.js

--- src/data/sceneLayout.js
+++ src/data/sceneLayout.js
@@ -2,11 +2,11 @@ export const SCENE_LAYOUT = {
   referenceWidth: 390,
   referenceHeight: 844,
   objects: [
-    { id: "sun", x: 37.667, y: 234.667, scale: { x: 0.054, y: 0.054 } },
-    { id: "moon", x: 37.666, y: 236, scale: { x: 0.05, y: 0.05 } },
-    { id: "magic_circle", x: 229.667, y: 665.999, scale: { x: 0.226, y: 0.226 } },
-    { id: "campfire", x: 79, y: 658, scale: { x: 0.095, y: 0.095 } },
-    { id: "crystal", x: 287, y: 566.666, scale: { x: 0.05, y: 0.05 } },
-    { id: "companion", x: 229, y: 660.667, scale: { x: 0.567, y: 0.567 } }
+    { id: "sun", x: 318.378, y: 229.264, scale: { x: 0.12, y: 0.12 } },
+    { id: "moon", x: 321.046, y: 228.07, scale: { x: 0.115, y: 0.115 } },
+    { id: "magic_circle", x: 221.889, y: 634.773, scale: { x: 0.129, y: 0.129 } },
+    { id: "campfire", x: 141.769, y: 639.274, scale: { x: 0.05, y: 0.05 } },
+    { id: "crystal", x: 313.769, y: 573.083, scale: { x: 0.05, y: 0.05 } },
+    { id: "companion", x: 224.487, y: 635.551, scale: { x: 0.445, y: 0.445 } }
   ]
 };
